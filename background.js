46d1d54a1e34ddc09ac03f4ce30a8b4c737a3643
Jonathan Leroy <jonathan@inikup.com>
2016-10-15 16:03:59 +0200

    Initial commit, version 1.0.0.

---
+++ background.js
@@ -0,0 +1,17 @@
+chrome.webRequest.onBeforeSendHeaders.addListener(
+    function(details) {
+        details.requestHeaders.push({
+            name: "X-Molotov-Agent",
+            value: "{\"app_id\":\"electron_app\",\"app_build\":1,\"app_version_name\":\"0.9.6\",\"type\":\"desktop\",\"os\":\"\",\"os_version\":\"\",\"manufacturer\":\"\",\"serial\":\"\",\"model\":\"\",\"brand\":\"\"}"
+        });
+        
+        return {requestHeaders: details.requestHeaders};
+    },
+    {
+        urls: [
+            "https://fapi.molotov.tv/v2/auth/login"
+        ],
+        types: ["xmlhttprequest"]
+    },
+    ["blocking", "requestHeaders"]
+);
