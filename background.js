5c179dda430804c0724346a22e09964146ea67ab
Jonathan Leroy <jonathan@inikup.com>
2017-03-18 17:29:59 +0100

    Add support for Molotov version 1.3.1.

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ chrome.webRequest.onBeforeSendHeaders.addListener(
     function(details) {
         details.requestHeaders.push({
             name: "X-Molotov-Agent",
-            value: "{\"app_id\":\"electron_app\",\"app_build\":3,\"app_version_name\":\"1.2.2\",\"type\":\"desktop\",\"electron_version\":\"1.4.12\",\"os\":\"Unknown\",\"os_version\":\"Unknown\",\"manufacturer\":\"MoloChrome\",\"serial\":\"Unknown\",\"model\":\"MoloChrome\",\"brand\":\"MoloChrome\"}"
+            value: "{\"app_id\":\"electron_app\",\"app_build\":3,\"app_version_name\":\"1.3.1\",\"type\":\"desktop\",\"electron_version\":\"1.4.12\",\"os\":\"Unknown\",\"os_version\":\"Unknown\",\"manufacturer\":\"MoloChrome\",\"serial\":\"Unknown\",\"model\":\"MoloChrome\",\"brand\":\"MoloChrome\"}"
         });
         
         return {requestHeaders: details.requestHeaders};
@@ -10,7 +10,8 @@ chrome.webRequest.onBeforeSendHeaders.addListener(
     {
         urls: [
             "https://fapi.molotov.tv/v2/auth/login",
-            "https://fapi.molotov.tv/v2/auth/refresh/*"
+            "https://fapi.molotov.tv/v2/auth/refresh/*",
+            "https://fapi.molotov.tv/v2/me/assets*"
         ],
         types: ["xmlhttprequest"]
     },
