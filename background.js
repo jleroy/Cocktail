fc8d3224e7d0bf95692f819aafafc4596fa405fe
Jonathan Leroy <jonathan@inikup.com>
2018-01-29 00:49:46 +0100

    Send "X-Molotov-Agent" header to new endpoints requiring it

--- background.js
+++ background.js
@@ -12,9 +12,13 @@ browser.webRequest.onBeforeSendHeaders.addListener(
     },
     {
         urls: [
+            "https://fapi.molotov.tv/v2/config",
+            "https://fapi.molotov.tv/v2/references",
+            "https://fapi.molotov.tv/v2/me/assets*",
+            "https://fapi.molotov.tv/v2/me/actions*",
+            "https://fapi.molotov.tv/v2/me/whats-up*",
             "https://fapi.molotov.tv/v3/auth/login",
             "https://fapi.molotov.tv/v3/auth/refresh/*",
-            "https://fapi.molotov.tv/v2/me/assets*",
             "https://multi-screen.molotov.tv/v3/ping*",
             "https://jessica.molotov.tv/video/preroll*"
         ],
